8aa226fbe3429aa775c0363023779c19187568d4
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-06-26 15:05:37 +0200

    bootloader-sunxi: fix unbootable image

--- scripts/bootloader-sunxi.sh
+++ scripts/bootloader-sunxi.sh
@@ -8,3 +8,7 @@ TARGET="/usr/lib/u-boot/$BOARD" u-boot-install-sunxi64 -f /tmp/bootsector.bin
 
 # Extract full u-boot binary
 dd if=/tmp/bootsector.bin of=/boot/u-boot-sunxi-with-spl.bin bs=8k skip=1
+
+# Rename kernel boot files so they're recognized by u-boot
+KERNEL_VERSION=`ls /lib/modules`
+/etc/kernel/postinst.d/zz-rename-files $KERNEL_VERSION
