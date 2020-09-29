07c77c38a5d922e0d0eb8049e8bae352feba0475
Erfan Abdi <erfangplus@gmail.com>
2020-09-29 23:40:42 +0330

    hybris-mobian: Initial bringup
    Co-authored-by: r3vn <giuseppe.corti@protonmail.com>

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ username=
 no_blockmap=
 ssh=
 
-while getopts "dDizobse:f:h:m:p:t:u:F:" opt
+while getopts "dDizobsre:f:h:m:p:t:u:F:" opt
 do
   case "$opt" in
     d ) use_docker=1 ;;
@@ -32,6 +32,7 @@ do
     b ) no_blockmap=1 ;;
     s ) ssh=1 ;;
     o ) installer=1 ;;
+    r ) rootfs=1 ;;
     f ) ftp_proxy="$OPTARG" ;;
     h ) http_proxy="$OPTARG" ;;
     m ) memory="$OPTARG" ;;
@@ -66,6 +67,16 @@ case "$device" in
     family="amd64"
     device="pc"
     ;;
+  "arm64-android9-generic" )
+    arch="arm64"
+    family="hybris"
+    device="android9-generic"
+    ;;
+  "armhf-android9-generic" )
+    arch="armhf"
+    family="hybris"
+    device="android9-generic"
+    ;;
   * )
     echo "Unsupported device '$device'"
     exit 1
@@ -76,6 +87,9 @@ image_file="mobian-$device-$environment-`date +%Y%m%d`.img"
 if [ "$installer" ]; then
   image="installer"
   image_file="mobian-installer-$device-$environment-`date +%Y%m%d`.img"
+fi
+if [ "$rootfs" ]; then
+  image_file="mobian-rootfs-$device-$environment-`date +%Y%m%d`.tar.gz"
 fi
 
 if [ "$use_docker" ]; then
@@ -129,6 +143,11 @@ fi
 
 if [ ! "$image_only" -o ! -f "rootfs-$device-$environment.tar.gz" ]; then
   $DEBOS_CMD $ARGS "rootfs-device.yaml" || exit 1
+fi
+
+if [ "$rootfs" ]; then
+  mv "rootfs-$device-$environment.tar.gz" "${image_file}"
+  exit 0
 fi
 
 $DEBOS_CMD $ARGS "$image.yaml"
