7f204a855d6d81398c9b88d96e2fd67d34717d57
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-09-29 11:38:39 +0200

    setup-repo: keep $http_proxy
    We no longer access the Mobian repo through HTTPS as it is now 
    GPG-signed, so we can keep http_proxy and only unset it when calling 
    extrepo.

--- scripts/setup-repo.sh
+++ scripts/setup-repo.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Our proxy is apt-cacher-ng, which can't handle SSL connections
-unset http_proxy
-
 # Allow non-free components
 sed -i '/non-free$/s/# //' /etc/extrepo/config.yaml
 
 # Install the Mobian repo
-extrepo enable mobian
+http_proxy= extrepo enable mobian
 
 # Setup repo priorities so mobian comes first
 cat > /etc/apt/preferences.d/00-mobian-priority << EOF
