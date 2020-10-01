210e8520a4764f2b91874708d6f44cd71e60e59e
Erfan Abdi <erfangplus@gmail.com>
2020-10-01 11:33:19 +0330

    hybris-mobian: Mask mobian-usb-gadget service

--- scripts/hybris-disable-services.sh
+++ scripts/hybris-disable-services.sh
@@ -2,6 +2,7 @@
 # disable mobian's MTP services since they break hybris-usb
 systemctl disable umtp-responder
 systemctl disable mobian-usb-gadget
+systemctl mask mobian-usb-gadget
 
 # disable fwupd, we don't need it
 systemctl disable fwupd
