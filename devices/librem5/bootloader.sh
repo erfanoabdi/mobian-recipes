12f2cb7b66fa171f3a72f39e48e8e598b3d61897
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-10-12 19:28:22 +0200

    librem5: bootloader: restore previous behavior
    debos doesn't seem to handle SSL connections well, don't push it too 
    hard.

--- devices/librem5/bootloader.sh
+++ devices/librem5/bootloader.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+wget -O /boot/librem5-boot.img \
+https://arm01.puri.sm/job/u-boot_builds/job/uboot_librem5_build/lastSuccessfulBuild/artifact/output/uboot-librem5/librem5-boot.img
+
 # Rename kernel boot files so they're recognized by u-boot
 KERNEL_VERSION=`ls /lib/modules`
 /etc/kernel/postinst.d/zz-rename-files $KERNEL_VERSION
