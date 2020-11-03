9711bf14f2196aef0c0be98018789d9c860167e4
Arnaud Ferraris <arnaud.ferraris@collabora.com>
2020-11-03 16:04:39 +0100

    sunxi: bootloader.sh: remove default config
    The u-boot-update config file is now in the pine*-tweaks packages.

--- devices/sunxi/bootloader.sh
+++ devices/sunxi/bootloader.sh
@@ -2,18 +2,6 @@
 
 BOARD="$1"
 
-cat > /etc/default/u-boot << EOF
-## /etc/default/u-boot - configuration file for u-boot-update(8)
-
-U_BOOT_UPDATE="true"
-U_BOOT_MENU_LABEL="Mobian GNU/Linux"
-U_BOOT_PARAMETERS="console=ttyS0,115200 consoleblank=0 loglevel=7 rw splash plymouth.ignore-serial-consoles vt.global_cursor_default=0"
-U_BOOT_TIMEOUT="10"
-U_BOOT_ROOT=""
-U_BOOT_FDT_DIR="/dtb-"
-
-EOF
-
 # Install u-boot to dummy bootsector
 dd if=/dev/zero of=/tmp/bootsector.bin bs=8k count=6
 TARGET="/usr/lib/u-boot/$BOARD" u-boot-install-sunxi64 -f /tmp/bootsector.bin
